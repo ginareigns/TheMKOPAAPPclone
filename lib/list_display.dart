1e077b1f5654e73cb8643f24cba1856b8ca9ff7c
nakabuukareginadesire <nakabuukareginadesire@gmail.com>
2024-11-19 08:57:31 +0000

    toast

--- lib/list_display.dart
+++ lib/list_display.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 // class to define a Todo
 class Todo {
@@ -17,6 +18,8 @@ class TodoListDisplay extends StatelessWidget {
     const Todo('Complete Flutter project', 'Finish the todo app by evening'),
     const Todo('Call mom', 'Catch up with mom and update her about your day'),
   ];
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +48,6 @@ class TodoListDisplay extends StatelessWidget {
                 style:
                     const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-
               onTap: () {
                 Navigator.push(
                   context,
@@ -66,6 +68,16 @@ class DetailsofTodo extends StatelessWidget {
   const DetailsofTodo({super.key, required this.todo});
 
   final Todo todo;
+  void _showToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,10 +85,31 @@ class DetailsofTodo extends StatelessWidget {
       appBar: AppBar(
         title: Text(todo.title),
       ),
-
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(todo.description),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(todo.description),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+              
+                onPressed: () {
+                  _showToast('You have tapped me');
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(300, 50),
+                  backgroundColor: const Color.fromARGB(255, 27, 170, 82),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                icon: const Icon(Icons.check),
+                label: const Text('Tap me', style: TextStyle(fontSize: 25),)),
+          ),
+        ],
       ),
     );
   }
